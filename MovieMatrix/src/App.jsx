0e86e68ca49b96751f74c49f8d03c9f58a1d09ef
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-06-06 00:41:12 +0530

    Update

--- MovieMatrix/src/App.jsx
+++ MovieMatrix/src/App.jsx
@@ -1,13 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const [movieName,setMoviename] = useState('');
+  const [searchResult,setSearchResult] = useState(null);
+ 
+
+  useEffect(()=>{
+    const fetchdata = async () =>{
+      if(movieName){
+        try {
+          const response = await axios.get(
+            `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieName}`
+          );
+          console.log(response.data)
+          setSearchResult(response.data);
+        } catch (error) {
+          console.error("Error fetching the move data " ,error)
+          setSearchResult(null);
+        }
+      }
+    }
+    fetchdata();
+  },[movieName])
+
+  const handleInputChanges = async (e) =>{
+    setMoviename(e.target.value)
+  }
+
+  const handleSearch = async (e) =>{
+    e.preventDefault();
+    
+  }
 
   return (
     <>
+    <div className='flex justify-center py-10'>
+      <form onSubmit={handleSearch}>
+      <input
+       value={movieName}
+       onChange={handleInputChanges}
+       placeholder='Search for movies'
+       className='border-solid border-2 border-indigo-600 py-2 px-4 rounded'/>
+
+    <button
+     type="submit"
+     className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Search</button>
+      </form>
+    </div>
+
+    <div className='card flex justify-center'>
+       {searchResult && (
+         <div>
+         <h2>Search Result</h2>
+          <p>Movie Name: {searchResult.Title}</p>
+          <p>Year: {searchResult.Year}</p>
+         <img src={searchResult.Poster}/>
+         </div>
+       )}
+    </div>
+    
      
     </>
   )
